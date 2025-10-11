893ca037592ea4174630c099fd65c0f262bd4fb3
Marcelo Gelato <gelatomarcelo@hotmail.com>
2025-10-11 15:10:33 -0300

    modificaciones y cambio templates .phtml

--- ejercicio5/app/tasks.php
+++ ejercicio5/app/tasks.php
@@ -1,26 +1,30 @@
 <?php
 // require_once 'db.php';
-require_once 'templates/form_add.php';
-require_once 'templates/tabla.php';
+require_once 'templates/form_add.phtml';
+require_once 'templates/tabla.phtml';
+
 
 function showHome() {
     showStuff();
 }
 
 function showStuff() {
-    require_once 'templates/header.php';
+    require_once 'templates/header.phtml';
     $materias = getStuff();
-    require_once 'templates/footer.php';
+    require_once 'templates/footer.phtml';
 }
 
 function addStuff() {
     // TODO: validacion de datos
-    if (isset($_POST['nombre'], $_POST['profesor']) && !empty($_POST['nombre'])) {
+    if (isset($_POST['nombre'], $_POST['profesor'], $_POST['carrera'], $_POST['anios'], $_POST['id']) && !empty($_POST['nombre'])) {
         // obtengo los datos del usuario
-        $nombre = $_POST['nombre'];
+        $id = $_POST['id'];
+        $nombre_materia = $_POST['nombre'];
         $profesor = $_POST['profesor'];
+        $nombre_carrera = $_POST['carrera'];
+        $anios = $_POST['anios'];
         // inserto en la DB
-        insertStuff($nombre, $profesor);
+        insertStuff($id, $nombre_materia, $profesor, $nombre_carrera, $anios);
         header('Location: ' . BASE_URL);
     } else {
         echo 'Error!';
@@ -28,7 +32,13 @@ function addStuff() {
 }
 
 function renewStuff($id) {
-    var_dump($id);
+    conectStuffById($id);
+    showEditForm($id);
+}
+
+function showEditForm($id) {
+    $data = getStuffById($id);
+    require_once 'templates/form_update.phtml';
 }
 
 function removeStuff($id) {
